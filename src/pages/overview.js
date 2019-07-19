338e1b535bdf3d4f3c5ce53ec858dac610bd3758
SimpsonThomas <simpsonthomas99@gmail.com>
2019-07-19 09:32:33 +0100

    Changed map and name

--- src/pages/overview.js
+++ src/pages/overview.js
@@ -46,7 +46,7 @@ class Overview extends Component {
 
         return (
             <div className='container text-center content-center'>
-                <h3>Inicident Dashboard Testing</h3>
+                <h3>Hermes Dashboard</h3>
                 <div className='row'>
                     <img src={preImage + "london-map.png"} height='500px' className='col-5' alt='Map of London'/>
                     <div className='col-1'/>
